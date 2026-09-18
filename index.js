b66ab4f0989c58e761f5f2bbfd881ca4b9d53e8b
Jaiden <jaidenwilson3280@gmail.com>
2026-09-17 20:01:14 -0400

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+
+const burgers = ["hamburger", "cheeseburger"] 
+
+let featuredDrink = "Strawberry Milkshake"
+
+function addBurger() {
+  const newBurger = "Flatburger"
+    burgers.push(newBurger);
+
+}
+
+if (true) {
+    const anotherNewburger = "Maple Bacon Burger"
+    burgers.push(anotherNewburger);
+}
+
+function changeFeaturedDrink() {
+    featuredDrink = "The JavaShake"
+        }
+        
